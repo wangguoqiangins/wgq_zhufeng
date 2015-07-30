51c58868775c14b55dbf4447d59a3d89978be2bc
Zhang Xiaohe <vhly@163.com>
2015-07-30 19:10:48 +0800

    添加实体

---
+++ app/src/main/java/com/qianfeng/zhufengfm/app/model/discover/SpecialColumn.java
@@ -0,0 +1,10 @@
+package com.qianfeng.zhufengfm.app.model.discover;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vhly[FR]
+ * Date: 15/7/30
+ * Email: vhly@163.com
+ */
+public class SpecialColumn {
+}
