66a2a5ad57fdb3ca23b0f7bbff808a30eb948f64
Zhang Xiaohe <vhly@163.com>
2015-07-31 12:13:43 +0800

    添加release发布信息

--- app/src/main/java/com/qianfeng/zhufengfm/app/SplashActivity.java
+++ app/src/main/java/com/qianfeng/zhufengfm/app/SplashActivity.java
@@ -47,10 +47,8 @@ public class SplashActivity extends FragmentActivity implements TaskCallback {
 
         // 启动扉页，进行网络检查与网络请求，下载数据
         // 最终显示主界面
-        int a = 3;
-        int b = 0;
-        int i = a / b;
-        MyLog.d("Splash", "3 / 0 = " + i);
+
+        MyLog.d("Splash", "3 / 0");
 
         CategoryTagMenuTask task =
                 new CategoryTagMenuTask(this);
--- app/src/main/java/com/qianfeng/zhufengfm/app/util/MyLog.java
+++ app/src/main/java/com/qianfeng/zhufengfm/app/util/MyLog.java
@@ -8,11 +8,18 @@ package com.qianfeng.zhufengfm.app.util;
  */
 
 import android.util.Log;
+import com.qianfeng.zhufengfm.app.BuildConfig;
 
 /**
  * 封装Android Log 工具，能够增加日志的开关
  */
 public final class MyLog {
+
+    /**
+     * 日志的开关，在Release(发布软件包)的时候 关闭日志
+     */
+    private static final boolean ON = BuildConfig.DEBUG;
+
     /**
      * 通过变量  true, false 进行日志输出控制
      */
@@ -20,19 +27,23 @@ public final class MyLog {
 
     private static final boolean INFO = true;
 
-    private MyLog(){
+    private MyLog() {
 
     }
 
-    public static void d(String tag, String msg){
-        if(DEBUG) {
-            Log.d(tag, msg);
+    public static void d(String tag, String msg) {
+        if (ON) {
+            if (DEBUG) {
+                Log.d(tag, msg);
+            }
         }
     }
 
-    public static void i(String tag, String msg){
-        if (INFO){
-            Log.i(tag, msg);
+    public static void i(String tag, String msg) {
+        if (ON) {
+            if (INFO) {
+                Log.i(tag, msg);
+            }
         }
     }
 
